080b18fbdfb43ca32c6ee7017c2986b94254fc2e
Solomon Hykes <solomon@dagger.io>
2026-04-08 00:54:32 -0700

    workspace: flatten migration plan and remove compat shims

--- engine/server/session_workspaces.go
+++ engine/server/session_workspaces.go
@@ -649,10 +649,7 @@ func (srv *Server) detectAndLoadWorkspaceWithRootfs(
 				Ref:               resolveLocalRef(ws, rel),
 				Name:              compatWorkspace.MainModule.Name,
 				Entrypoint:        compatWorkspace.MainModule.Entry.Entrypoint,
-				legacyFieldPolicy: legacyWorkspaceFieldPolicyDirect,
-			}
-			if compatWorkspace.MainModule.StripLegacyWorkspaceFields {
-				mod.legacyFieldPolicy = legacyWorkspaceFieldPolicyStripCompatMain
+				legacyFieldPolicy: legacyWorkspaceFieldPolicyStripCompatMain,
 			}
 			pending = append(pending, mod)
 		}
